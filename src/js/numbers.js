a04c1b663ae803f48c24a5617606a568eff081c8
TheUnterhemd <kodel.thomas83@gmail.com>
2023-01-11 15:29:56 +0100

    onload

--- src/js/numbers.js
+++ src/js/numbers.js
@@ -7,30 +7,28 @@ export let artists = {
 
 // PLAYLISTS
 
-export let playlists = {
-    popRock: {
+export let playlists = [ {
         creator: "Rod",
         number: 8074581462
     },
 
-    softPop: {
+    {
         creator: "Fabio",
         number: 1282523285
     },
 
-    party: {
+    {
         creator: "Alice",
         number: 2097558104
     },
 
-    alternative: {
+    {
         creator: "George",
         number: 5337198442
     },
 
-    chill: {
+    {
         creator: "Alice",
         number: 3338949242
     },
-
-}
+]
