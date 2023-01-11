11b3c8c24e6fe283b3b9977db729e1b48f438f58
TheUnterhemd <kodel.thomas83@gmail.com>
2023-01-11 14:48:27 +0100

    onload element

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import {artists} from "./numbers.js"
 import { api } from "./env.js";
 
+// OPTIONS for APIs
 const optionsDeezer = {
 	method: 'GET',
 	headers: {
@@ -9,15 +10,44 @@ const optionsDeezer = {
 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
 	}
 };
+
+//URLs für DEEZER API
 const urlArtist = `https://deezerdevs-deezer.p.rapidapi.com/artist/${artists.käptnPeng.number}`
+const urlPlaylist = `https://deezerdevs-deezer.p.rapidapi.com/playlist/8074581462`
 
 
+//FETCH FUNKTION
 const getData = async (url, options) => {
     let response = await fetch(url,options);
     console.log(response);
     let data = await response.json();
     console.log(data);
+	return data;
 };
-console.log(artists);
-getData(urlArtist, optionsDeezer);
+
+
+// DOM connection
+
+let startPlaylist = document.getElementById('playlistsStart');
+
+
+//ONLOAD
+
+window.onload = () => {
+	
+	startPlaylist.innerHtml = `
+	<div class="list">
+	<div class="item">
+	  <img src="/img/among_us_cover.jpg" />
+	  <div class="play">
+		<span class="fa fa-play"></span>
+	  </div>
+	  <h4>Today's Top Hits</h4>
+	  <p>Rema & Selena Gomez are on top of the...</p>
+	</div>`;
+};
+
+
+
+
 
