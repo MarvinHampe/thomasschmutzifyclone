5efeebf503f212c7de1778b20d325a14a7fc9dea
TheUnterhemd <kodel.thomas83@gmail.com>
2023-01-15 14:25:34 +0100

    rewrote index.js --> onLoad

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
 // import * as bootstrap from '../../node_modules/bootstrap/js';
 import {
-	artists,
-	playlists,
-	albums
-} from "./numbers.js"
-import {
 	api
 } from "./env.js";
 
@@ -31,11 +26,64 @@ const url = `https://deezerdevs-deezer.p.rapidapi.com/`
 const getData = async (endpoint) => {
 
 	let response = await fetch(url + endpoint, options);
-	console.log(response);
+	// console.log(response);
 	let data = await response.json();
 	console.log(data);
 	return data;
 };
+
+// Randomizer
+
+const randomizer = (limit) => Math.floor(Math.random()*limit);
+
+// OnLoad function
+
+const load = async (endpoint,limit) =>{
+
+	for(let i = 0; i < 5; i++) {
+
+	let data = await getData(endpoint+randomizer(limit));
+	console.log(endpoint);
+		
+		if(endpoint == "playlist/")
+				{startPlaylist.innerHTML += `
+					<div class="list">
+					<div class="item">
+	  				<img src="${data.picture_big}" />
+	  				<div class="play">
+					<span class="fa fa-play" id='${data.id}'></span>
+	  				</div>
+	  				<h4>${data.title}</h4>
+	  				<p>${data.description}</p>
+					</div>`;}
+		else if(endpoint == "artist/"){
+
+			startArtist.innerHTML += `
+		<div class="list">
+		<div class="item">
+		  <img src="${data.picture_big}" />
+		  <div class="play">
+			<span class="fa fa-play"></span>
+		  </div>
+		  <h4>${data.name}</h4>
+		  <p>${data.type}</p>
+		</div>`;
+
+		}
+		else if(endpoint == "album/"){
+			startPodcast.innerHTML += `
+		<div class="list">
+		<div class="item">
+		  <img src="${data.cover_big}" />
+		  <div class="play">
+			<span class="fa fa-play"></span>
+		  </div>
+		  <h4>${data.title}</h4>
+		  <p>{data.artist.name} ---> wird nicht übernommen</p>
+		  
+		</div>`;}
+
+}}
 
 
 // DOM connection
@@ -51,132 +99,85 @@ let mainContainer = document.getElementById('main-container');
 
 window.onload = () => {
 
-	playlists.forEach(async (playlist) => {
-		const data = await getData(`playlist/${playlist.number}`);
-		console.log(data);
+	
+		load("playlist/",1000);
+		load("artist/",99999);
+		load("album/",99999);}
 
-		startPlaylist.innerHTML += `
-		<div class="list">
-		<div class="item">
-		  <img src="${data.picture_big}" />
-		  <div class="play">
-			<span class="fa fa-play" id='${data.id}'></span>
-		  </div>
-		  <h4>${data.title}</h4>
-		  <p>${data.description}</p>
-		</div>`;
 
-		//Only the first item works
-
-		document.querySelectorAll('.fa-play').forEach(list => {
-			return list.addEventListener('click', () => {
-				console.log("hi");
+		// document.querySelectorAll('.fa-play').forEach(list => {
+		// 	return list.addEventListener('click', () => {
+		// 		console.log("hi");
 
 
 
-				mainContainer.innerHTML = `
-				<div class="container">
-					<div class="header">
-						<div class="image_container">
-							<img src="${data.picture_big}" />
-						</div>
-						<div class="data_container">
-							<h1>${data.title}</h1>
-							<div class="small_details">
-								<h2 class="creator">${data.creator.name}</h2>
-								<p class="playlist_description">${data.description}</p>
-							</div>
-							<ul class="data">
-								<li>${data.type}</li>
-								<li>Duration: ${data.duration}</li>
-								<li>Fans: ${data.fans}</li>
-								<li>Number of songs: ${data.nb_tracks}</li>
-							</ul>
-							<div class="small_details">
-								<button>Listen</button>
-								<i>Heart Icon: ${data.is_loved_track}</i>
-								<i>Share icon</i>
-							</div>
-						</div>
-						<div class="track-list">
-							<div class="first-line">
-								<span class="span-title">TITLE</span>
-								<span class="span-artist">ARTIST</span>
-								<span class="span-album">ALBUM</span>
-								<span class="span-date">DATE</span>
-								<span class="span-duration">DURATION ICON</span>
-							</div>
+		// 		mainContainer.innerHTML = `
+		// 		<div class="container">
+		// 			<div class="header">
+		// 				<div class="image_container">
+		// 					<img src="${data.picture_big}" />
+		// 				</div>
+		// 				<div class="data_container">
+		// 					<h1>${data.title}</h1>
+		// 					<div class="small_details">
+		// 						<h2 class="creator">${data.creator.name}</h2>
+		// 						<p class="playlist_description">${data.description}</p>
+		// 					</div>
+		// 					<ul class="data">
+		// 						<li>${data.type}</li>
+		// 						<li>Duration: ${data.duration}</li>
+		// 						<li>Fans: ${data.fans}</li>
+		// 						<li>Number of songs: ${data.nb_tracks}</li>
+		// 					</ul>
+		// 					<div class="small_details">
+		// 						<button>Listen</button>
+		// 						<i>Heart Icon: ${data.is_loved_track}</i>
+		// 						<i>Share icon</i>
+		// 					</div>
+		// 				</div>
+		// 				<div class="track-list">
+		// 					<div class="first-line">
+		// 						<span class="span-title">TITLE</span>
+		// 						<span class="span-artist">ARTIST</span>
+		// 						<span class="span-album">ALBUM</span>
+		// 						<span class="span-date">DATE</span>
+		// 						<span class="span-duration">DURATION ICON</span>
+		// 					</div>
 						
-							<div class="track-container">
+		// 					<div class="track-container">
 							
-							 ${data.tracks.data.map(data => {
-								// FIND A WAY TO EDIT THE HTML WITH THE TRACK INFO
+		// 					 ${data.tracks.data.map(data => {
+		// 						FIND A WAY TO EDIT THE HTML WITH THE TRACK INFO
 								
-								// document.querySelector('.track-container').innerHTML= `
+		// 						document.querySelector('.track-container').innerHTML= `
 								
-								// <div class="track">
-								// 	<div class="track-image">
-								// 		<img src="${data.album.cover_big}" />
-								// 	</div>
-								// 	<div class="track-title">
-								// 		<span>${data.title}"<span>
-								// 	</div>
-								// 	<div class="track-artist">
-								// 		<span>${data.artist.name}"<span>
-								// 	</div>
-								// 	<div class="track-album">
-								// 		<span>${data.album.title}"<span>
-								// 	</div>
-								// </div>
+		// 						<div class="track">
+		// 							<div class="track-image">
+		// 								<img src="${data.album.cover_big}" />
+		// 							</div>
+		// 							<div class="track-title">
+		// 								<span>${data.title}"<span>
+		// 							</div>
+		// 							<div class="track-artist">
+		// 								<span>${data.artist.name}"<span>
+		// 							</div>
+		// 							<div class="track-album">
+		// 								<span>${data.album.title}"<span>
+		// 							</div>
+		// 						</div>
 								
-								// `
-								 return data.title;
-							 })}
-							</div>
+		// 						`
+		// 						 return data.title;
+		// 					 })}
+		// 					</div>
 						
 						
 						
 						
 						
-						</div>
-					</div>
-				</div>
-				`
-			})
-		})
-
-	})
-
-	artists.map(async (artist) => {
-		const data = await getData(`artist/${artist.number}`);
-		console.log(data);
-
-		startArtist.innerHTML += `
-		<div class="list">
-		<div class="item">
-		  <img src="${data.picture_big}" />
-		  <div class="play">
-			<span class="fa fa-play"></span>
-		  </div>
-		  <h4>${data.name}</h4>
-		  <p>${data.type}</p>
-		</div>`;
-	})
-
-	albums.map(async (album) => {
-		const data = await getData(`album/${album.number}`);
-		console.log(data);
-
-		startPodcast.innerHTML += `
-		<div class="list">
-		<div class="item">
-		  <img src="${data.cover_big}" />
-		  <div class="play">
-			<span class="fa fa-play"></span>
-		  </div>
-		  <h4>${data.title}</h4>
-		  <p>${data.artist.name}</p>
-		</div>`;
-	})
-
-};
+		// 				</div>
+		// 			</div>
+		// 		</div>
+		// 		`
+		// 	})
+		// })
